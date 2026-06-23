c2359928b88f2036fbb6b7b2bb4ad79cd3e69adc
ferdyferdyan <id.umrah@gmail.com>
2026-06-23 14:41:10 +0700

    Add files via upload

---
+++ server_list.inc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @File name : server_list.inc.php
+ * Daftar Server SLiMS Target untuk Plugin SLiMS Copy
+ */
+
+// Reset array agar bersih dari daftar bawaan
+$sysconf['marc_XML_source'] = array();
+
+// Daftar server target
+$sysconf['marc_XML_source'][] = array('uri' => 'https://koleksi.perpusblam.id/', 'desc' => 'Perpustakaan Balai Litbang Agama Makassar');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://opac.ushuluddin.uinjkt.ac.id/', 'desc' => 'OPAC Ushuluddin UIN Jakarta');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://opac.usk.ac.id/', 'desc' => 'Perpustakaan Universitas Syiah Kuala');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://perpustakaan.kkp.go.id/', 'desc' => 'Knowledge Repository KKP');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://perpustakaan.kemendagri.go.id/opac/', 'desc' => 'Perpustakaan Kemendagri');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://perpustakaan.kemlu.go.id/', 'desc' => 'Perpustakaan Kemlu');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://mpn.komdigi.go.id/perpus/', 'desc' => 'Perpustakaan Komdigi');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://library.isi-padangpanjang.ac.id/', 'desc' => 'ISI Padang Panjang');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://opac.dpr.go.id/', 'desc' => 'DPR');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://library.binahusada.ac.id/', 'desc' => 'Bina Husada');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://pustaka-bpkxii.org/slims', 'desc' => 'Pustaka BPK XII');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://perpustakaan.anri.go.id/index.php', 'desc' => 'ANRI');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://perpustakaan.ici.ac.id/', 'desc' => 'Citra Nasional');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://elibrary.poltektedc.ac.id/', 'desc' => 'Perpustakaan Politeknik TEDC');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://lib.universitas-bth.ac.id/index.php', 'desc' => 'Bakti Husada');
+$sysconf['marc_XML_source'][] = array('uri' => 'https://opac.peradaban.ac.id/', 'desc' => 'Perpustakaan Peradaban');
+
+?>
